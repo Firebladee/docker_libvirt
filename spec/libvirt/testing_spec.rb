476bb0963ca4113de94fa91a8bc6229b55d7b02a
Firebladee <mgreenha@yahoo.co.uk>
2020-09-04 15:01:20 +0100

    Update concourse

--- spec/libvirt/testing_spec.rb
+++ spec/libvirt/testing_spec.rb
@@ -4,7 +4,7 @@ require 'serverspec'
 require 'docker-api'
 
 describe 'Dockerfile' do
-  image = Docker::Image.build_from_dir('./systemd')
+  image = Docker::Image.build_from_dir('.')
   set :os, family: :redhat
   set :backend, :docker
   set :docker_image, image.id
