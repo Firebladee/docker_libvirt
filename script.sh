91f74e8938e3db1a2891c63a9420a4a9e3f87c2b
Firebladee <mgreenha@yahoo.co.uk>
2020-11-05 09:59:13 +0000

    Added verbose to script

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-printf "\nInstall libvirt\n"
-
 printf "\nStart libvirtd\n"
 systemctl enable libvirtd
 systemctl start libvirtd
@@ -9,6 +7,6 @@ systemctl start libvirtd
 export LIBGUESTFS_BACKEND=direct
 
 printf "\nConverting vm to tar file\n"
-virt-tar-out -a CentOS-7-x86_64-GenericCloud-1711.qcow2  / - | gzip --best > centos7.tgz
+virt-tar-out --verbose -a CentOS-7-x86_64-GenericCloud-1711.qcow2  / - | gzip --best > centos7.tgz
 
 # TODO: Variablize this script
